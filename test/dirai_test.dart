b17766b7c985a566795446e264d158cb5cd38315
AHpx <AHpx@yandex.com>
2022-04-04 21:35:24 +0800

    Removed tests

--- test/dirai_test.dart
+++ test/dirai_test.dart
@@ -1,16 +1,15 @@
-import 'package:dirai/dirai.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+
 
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+
     });
   });
 }
