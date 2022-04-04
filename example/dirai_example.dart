f38540b8bf01c7772f3b372b38a881d00c3a70da
AHpx <AHpx@yandex.com>
2022-04-04 20:38:14 +0800

    Initializing commit

---
+++ example/dirai_example.dart
@@ -0,0 +1,6 @@
+import 'package:dirai/dirai.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dirai.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dirai;
+
+export 'src/dirai_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
